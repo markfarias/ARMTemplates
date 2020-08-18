d80b3e1fa2295e4eceaee9391e9b6a6a24f6094c
markfarias <mark.farias@asmr.com>
2020-08-18 18:40:48 -0400

    Add files via upload
    Added new content to script

--- ps/CommonSqlServers.ps1
+++ ps/CommonSqlServers.ps1
@@ -49,13 +49,32 @@ if($drv) {
 }
 
 ## Rename the SQL Server instance computer name
-$newname = $env:COMPUTERNAME
 Start-Sleep -s 45
+$newname = $env:userdomain
 $command = @"
-    sp_dropserver @@servername; 
-    GO
-    sp_addserver '{0}',local;
-    GO
+       sp_dropserver @@servername; 
+       GO
+       sp_addserver '{0}',local;
+       GO
+
+declare @CodeNameString  nvarchar(max)
+set @CodeNameString = null
+SELECT @CodeNameString = Coalesce(@CodeNameString + '; ', '') + 'IF NOT EXISTS (SELECT * FROM master.sys.server_principals WHERE [name] = ''' + [name] + ''')
+CREATE LOGIN [{0}\' + right(name, len(name) - charindex('\', name) ) + '] FROM WINDOWS WITH DEFAULT_DATABASE=[' + 
+        default_database_name + '], DEFAULT_LANGUAGE=[us_english]'
+FROM master.sys.server_principals
+where type_desc In ('WINDOWS_GROUP', 'WINDOWS_LOGIN')
+AND [name] not like 'BUILTIN%'
+and [NAME] not like 'NT %'
+and [name] not like '%\SQLServer%'
+SELECT @CodeNameString = Coalesce(@CodeNameString + '; ', '') + 'ALTER SERVER ROLE [' + r.name + '] ADD MEMBER [{0}\' + right(l.name, len(l.name) - charindex('\', l.name) ) + ']' 
+from master.sys.server_role_members rm
+join master.sys.server_principals r on r.principal_id = rm.role_principal_id
+join master.sys.server_principals l on l.principal_id = rm.member_principal_id
+where l.[name] not in ('sa')
+AND l.[name] not like 'BUILTIN%'
+and l.[NAME] not like 'NT %'
+exec sp_executesql @CodeNameString
 "@ -f $newname
 
-Invoke-Sqlcmd -ServerInstance "." -Query $command
+Invoke-Sqlcmd -ServerInstance "." -Query $command 
